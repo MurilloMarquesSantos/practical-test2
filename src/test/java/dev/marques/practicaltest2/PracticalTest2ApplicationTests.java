8d2cb36fb5933bf8cc2f1b31f794ec8cefa706f3
Murillo Marques <murillomarques2001@gmail.com>
2025-08-29 13:10:31 -0300

    First commit

---
+++ src/test/java/dev/marques/practicaltest2/PracticalTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marques.practicaltest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
