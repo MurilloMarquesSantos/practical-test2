0332bd021e344beab5de78a755ba199c37fdd457
Murillo Marques <murillomarques2001@gmail.com>
2025-08-29 13:26:08 -0300

    feat: adiciona service

---
+++ src/main/java/dev/marques/practicaltest2/service/BookService.java
@@ -0,0 +1,76 @@
+package dev.marques.practicaltest2.service;
+
+import dev.marques.practicaltest2.client.OpenLibraryClient;
+import dev.marques.practicaltest2.dto.BookDto;
+import dev.marques.practicaltest2.dto.PagedResponse;
+import dev.marques.practicaltest2.dto.SearchResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class BookService {
+
+    private final OpenLibraryClient client;
+
+    public BookService(OpenLibraryClient client) {
+        this.client = client;
+    }
+
+    public PagedResponse<BookDto> searchBySubject(String subject, int page, int size, boolean sortAsc) {
+
+        int olPage = Math.max(1, page);
+        SearchResponse resp = client.search("subject:" + subject, olPage, size, null);
+
+        List<BookDto> list = Optional.ofNullable(resp.getDocs()).orElse(List.of())
+                .stream()
+                .map(d -> new BookDto(d.getTitle(), d.getAuthorName(), d.getFirstPublishYear(), d.getKey()))
+                .collect(Collectors.toList());
+
+        Comparator<BookDto> cmp = Comparator.comparing(b -> Optional.ofNullable(b.getTitle()).orElse(""));
+        if (!sortAsc) cmp = cmp.reversed();
+        list.sort(cmp);
+
+        return new PagedResponse<>(
+                list,
+                page,
+                size,
+                resp.getNumFound()
+        );
+    }
+
+    public PagedResponse<BookDto> searchByTitle(String title, int page, int size, boolean sortAsc) {
+        int olPage = Math.max(1, page);
+
+        SearchResponse resp = client.search("title:" + title, olPage, size, null);
+
+        List<BookDto> list = Optional.ofNullable(resp.getDocs()).orElse(List.of())
+                .stream()
+                .map(d -> new BookDto(
+                        d.getTitle(),
+                        d.getAuthorName(),
+                        d.getFirstPublishYear(),
+                        d.getKey()
+                ))
+                .collect(Collectors.toList());
+
+        Comparator<BookDto> cmp = Comparator.comparing(
+                b -> Optional.ofNullable(b.getTitle()).orElse(""),
+                String.CASE_INSENSITIVE_ORDER
+        );
+        if (!sortAsc) {
+            cmp = cmp.reversed();
+        }
+        list.sort(cmp);
+
+        return new PagedResponse<>(
+                list,
+                page,
+                size,
+                resp.getNumFound()
+        );
+    }
+}
