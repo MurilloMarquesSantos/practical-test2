0c203b2e5c9cc98106754a51049597d59ebc6d47
Murillo Marques <murillomarques2001@gmail.com>
2025-08-29 13:19:06 -0300

    feat: adiciona client

---
+++ src/main/java/dev/marques/practicaltest2/client/OpenLibraryClient.java
@@ -0,0 +1,40 @@
+package dev.marques.practicaltest2.client;
+
+import dev.marques.practicaltest2.dto.SearchResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class OpenLibraryClient {
+
+    private final RestTemplate restTemplate;
+
+    private final String baseUrl;
+
+    public OpenLibraryClient(@Value("${open-library.base-url}") String baseUrl, RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+        this.baseUrl = baseUrl;
+    }
+
+    public SearchResponse search(String q, Integer page, Integer limit, String sort) {
+        UriComponentsBuilder uri = UriComponentsBuilder.fromHttpUrl(baseUrl + "/search.json")
+                .queryParam("q", q);
+        if (page != null) uri.queryParam("page", page);
+        if (limit != null) uri.queryParam("limit", limit);
+        if (sort != null) uri.queryParam("sort", sort);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        ResponseEntity<SearchResponse> resp =
+                restTemplate.exchange(uri.toUriString(), HttpMethod.GET, entity, SearchResponse.class);
+        return resp.getBody();
+    }
+}
