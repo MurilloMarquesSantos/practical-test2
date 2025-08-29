0d1cd9c2fc48167705d2ec7831ac1e413b539282
Murillo Marques <murillomarques2001@gmail.com>
2025-08-29 13:25:52 -0300

    feat: adiciona dto

---
+++ src/main/java/dev/marques/practicaltest2/dto/PagedResponse.java
@@ -0,0 +1,50 @@
+package dev.marques.practicaltest2.dto;
+
+import java.util.List;
+
+public class PagedResponse<T> {
+    private List<T> items;
+    private int page;
+    private int size;
+    private long totalElements;
+
+    public PagedResponse(List<T> items, int page, int size, long totalElements) {
+        this.items = items;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+    }
+
+    // Getters e setters
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void setItems(List<T> items) {
+        this.items = items;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+}
