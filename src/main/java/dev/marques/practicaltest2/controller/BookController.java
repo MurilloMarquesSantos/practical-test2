97049f3ba1c6ed43388f9fa6e161132708761f7a
Murillo Marques <murillomarques2001@gmail.com>
2025-08-29 13:30:51 -0300

    feat: adiciona método

--- src/main/java/dev/marques/practicaltest2/controller/BookController.java
+++ src/main/java/dev/marques/practicaltest2/controller/BookController.java
@@ -3,6 +3,7 @@ package dev.marques.practicaltest2.controller;
 import dev.marques.practicaltest2.dto.BookDto;
 import dev.marques.practicaltest2.dto.PagedResponse;
 import dev.marques.practicaltest2.service.BookService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,11 +21,28 @@ public class BookController {
     }
 
     @GetMapping("/comics")
-    public PagedResponse<BookDto> comics(@RequestParam(defaultValue = "1") int page,
-                                         @RequestParam(defaultValue = "20") int size,
-                                         @RequestParam(defaultValue = "true") boolean sortAsc) {
-        return bookService.searchBySubject("comics", page, size, sortAsc);
+    public ResponseEntity<PagedResponse<BookDto>> comics(@RequestParam(defaultValue = "1") int page,
+                                                         @RequestParam(defaultValue = "20") int size,
+                                                         @RequestParam(defaultValue = "true") boolean sortAsc) {
+        return ResponseEntity.ok(bookService.searchBySubject("comics", page, size, sortAsc));
 
+    }
+
+    @GetMapping("/movies")
+    public PagedResponse<BookDto> movies(
+            @RequestParam String name,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(defaultValue = "true") boolean sortAsc) {
+        return bookService.searchByTitle(name + " movie", page, size, sortAsc);
+    }
+
+    @GetMapping("/superheroes")
+    public PagedResponse<BookDto> superheroes(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(defaultValue = "true") boolean sortAsc) {
+        return bookService.searchBySubject("superheroes", page, size, sortAsc);
     }
 
 }
